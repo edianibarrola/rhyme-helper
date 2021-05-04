0f6ace5d98b627879a263dcc0cfda060249563b6
edianibarrola <edianibarrola@gmail.com>
2021-05-04 18:13:49 +0000

    update

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,12 +5,30 @@ import "../../styles/home.scss";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
-	const poets = ["2pac", "Shakespeare", "Notorious B.I.G.", "MF Doom", "Harry Mack", "Maya Angelou", "Robert Frost"];
+	const poets = [
+		"2pac",
+		"Shakespeare",
+		"Notorious B.I.G.",
+		"MF Doom",
+		"Harry Mack",
+		"Eminem",
+		"Maya Angelou",
+		"Robert Frost",
+		"Sarah Teasedale",
+		"Walt Whitman"
+	];
+	let poet = "none";
+	const randomPoet = poets.map(curr => {
+		poet = curr;
+		console.log(curr);
+		return poet;
+	});
+
 	return (
 		<div className="text-center mt-5">
 			<h1>rhym:Ed</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				<h1>{randomPoet}</h1>
 			</p>
 
 			<p>Page under construction... check back soon!</p>
